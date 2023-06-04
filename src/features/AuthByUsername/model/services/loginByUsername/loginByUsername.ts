2db10817430fea17894f2ae354427321cacfb6cf
Chatterbox777 <chatterbox1995@mail.ru>
2023-06-04 19:55:43 +0300

    extra thunApi arg

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { ThunkConfig } from "app/providers/StoreProvider/config/StateSchema";
 import { User, userActions } from "entities/User";
 import { USER_LOCAL_STORAGE_KEY } from "shared/const/localstorage";
 import { getErrorMessage } from "shared/lib/hooks";
@@ -12,20 +12,21 @@ interface LoginByUserNameProps {
 export const loginByUsername = createAsyncThunk<
   User,
   LoginByUserNameProps,
-  { rejectValue: string }
+  ThunkConfig<string>
 >(
   "loginForm/loginByUsername",
-  async ({ username, password }, { dispatch, rejectWithValue }) => {
+  async (authData, { dispatch, rejectWithValue, extra }) => {
     try {
-      const response = await axios.post("http://localhost:8000/login", {
-        username,
-        password,
-      });
+      const response = await extra.api.post<User>("/login", authData);
       if (!response.data) {
         throw new Error("empty data");
       }
-      localStorage.setItem(USER_LOCAL_STORAGE_KEY, JSON.stringify(response.data));
+      localStorage.setItem(
+        USER_LOCAL_STORAGE_KEY,
+        JSON.stringify(response.data)
+      );
       dispatch(userActions.setAuthData(response.data));
+      extra.navigate("/about");
 
       return response.data;
     } catch (error) {
